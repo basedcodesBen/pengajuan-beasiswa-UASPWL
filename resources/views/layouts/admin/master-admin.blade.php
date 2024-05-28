00a0f58a1d470ac6bc21876387972c86ff94eb74
basedcodesBen <benaya.andrias@gmail.com>
2024-05-28 21:19:11 +0700

    added plugins and some routes

---
+++ resources/views/layouts/admin/master-admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Administrasi Pengajuan Beasiswa</title>
+    <script src="{{ asset('https://cdn.tailwindcss.com') }}"></script>
+    <link href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css') }}" rel="stylesheet" />
+    {{-- <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css') }}">
+</head>
+<body class="bg-gray-100">
+    <div class="wrapper">
+        @include('layouts.admin.sidebar-admin')
+
+        <div class="content-wrapper">
+            @yield('web-content')
+        </div>
+    </div>
+
+      {{-- Scripts --}}
+      <script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/login/login-page');
 });
+
+Route::get('/admin-page', function () {
+    return view('pages/admin/index-admin');
+});
+
+Route::get('/admin/welcome', [AdminController::class, 'welcome'])->name('admin.welcome');
+Route::get('/admin/proposals', [AdminController::class, 'proposals'])->name('admin.proposals');
+Route::get('/admin/students', [AdminController::class, 'students'])->name('admin.students');
+Route::get('/admin/settings', [AdminController::class, 'settings'])->name('admin.settings');
