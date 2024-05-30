3989e0998dfec6b31b9913514aa7838a40dc3d00
basedcodesBen <benaya.andrias@gmail.com>
2024-05-30 08:29:26 +0700

    bug fixes

--- resources/views/pages/admin/index-admin.blade.php
+++ resources/views/pages/admin/index-admin.blade.php
@@ -6,9 +6,9 @@
         <p class="text-gray-600 mb-6">Manage scholarship proposals and student submissions efficiently.</p>
         
         <div class="flex flex-col space-y-4">
-            {{-- <a href="{{ route('admin.proposals') }}" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">View Proposals</a>
-            <a href="{{ route('admin.students') }}" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Manage Students</a>
-            <a href="{{ route('admin.settings') }}" class="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">Settings</a> --}}
+            <a href="{{ route('admin.proposals') }}" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Pengajuan Beasiswa</a>
+            <a href="{{ route('admin.students') }}" class="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Kelola Pengguna</a>
+            <a href="{{ route('admin.settings') }}" class="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600">Settings</a>
         </div>
     </div>
 </div>
