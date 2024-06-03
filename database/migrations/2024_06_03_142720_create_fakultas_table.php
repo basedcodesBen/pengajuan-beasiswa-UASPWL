158a0156233015cbde70f8757c1810673fcd4658
basedcodesBen <benaya.andrias@gmail.com>
2024-06-04 00:21:33 +0700

    added migrations

--- database/migrations/2024_06_03_142720_create_fakultas_table.php
+++ database/migrations/2024_06_03_142720_create_fakultas_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('fakultas', function (Blueprint $table) {
+            $table->string('id_fakultas')->primary();
+            $table->string('nama_fakultas');
             $table->timestamps();
         });
     }
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('fakultas');
     }
 };
